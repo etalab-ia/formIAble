0fc96fd3013d04714cf616021bd43bc9d324a343
EtiMag <etienne.maeght@gmail.com>
2023-05-16 18:48:25 +0200

    Check paddleocr installation

---
+++ src/models/classify-form/PaddleOCR&TextMatch/check-paddleocr-installation.py
@@ -0,0 +1,13 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+logging.info("Checking paddleocr installation ...")
+
+import paddleocr  # noqa: E402
+
+image_path: str = "data/synthetic_forms/cerfa_12485_03_fake1.jpg"
+
+ocrModel = paddleocr.PaddleOCR(use_angle_cls=False, lang='fr')
+ocrResult: list[list[list[list[float]] | tuple[str, float]]] = ocrModel.ocr(image_path, cls=False)
+
+logging.info("Checking paddleocr installation [OK]")
