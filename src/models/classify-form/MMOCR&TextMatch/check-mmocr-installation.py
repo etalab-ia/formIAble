c7867e7b6bba77d3b9fd0c5ee486442f49cf0cf4
EtiMag <etienne.maeght@gmail.com>
2023-05-21 20:38:23 +0200

    check mmocr installation

--- src/models/classify-form/MMOCR&TextMatch/check-mmocr-installation.py
+++ src/models/classify-form/MMOCR&TextMatch/check-mmocr-installation.py
@@ -1,5 +1,11 @@
+import logging
+import os
+import torch.hub
 from mmocr.apis import MMOCRInferencer
+logging.basicConfig(level=logging.DEBUG)
 
+logging.debug(torch.hub.get_dir())  # look here if download fails
+logging.debug(os.getcwd())
 
 ocr = MMOCRInferencer(det='DBNet', rec='CRNN')
-ocr('demo/demo_text_ocr.jpg', show=True, print_result=True)
+ocr('data/synthetic_forms/cerfa_12485_03_fake1.jpg', show=True, print_result=True)
